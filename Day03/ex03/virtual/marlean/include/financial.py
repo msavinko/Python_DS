52e540310226b348e774250876bedb757e9ed177
marlean <marlean@student.21-school.ru>
2022-09-13 13:27:10 +0300

    upd

--- Day03/ex03/virtual/marlean/include/financial.py
+++ Day03/ex03/virtual/marlean/include/financial.py
@@ -7,25 +7,42 @@ import requests
 
 def parse_info():
 	time.sleep(5)
-	print('LETS PARSE')
-	url = f'https://finance.yahoo.com/quote/{sys.argv[1]}/financials'
-	website = requests.get(url, headers={'User-Agent' : 'Custom'}) #Принимаем адрес с параметрами
+	print('~ Let\'s parse finance.yahoo webpage ~')
+	#Принимаем адрес и заголовки. Заголовки используются для того, чтобы клиент и сервер понимали, как интерпретировать данные, отправляемые и получаемые в запросе и ответе.
 	
-
-
 	# website = requests.get(f"https://finance.yahoo.com/quote/{sys.argv[1]}/financials", headers={'User-Agent' : 'Custom'})
+	# OR
+	# url = f'https://finance.yahoo.com/quote/{sys.argv[1]}/financials'
+	# website = requests.get(url, headers={'User-Agent' : 'Custom user agent'})
+	# OR
+	url = f'https://finance.yahoo.com/quote/{sys.argv[1]}/financials'
+	headers={'User-Agent': 'Custom user agent'}
+	website = requests.get(url, headers=headers)
 
 	if website.status_code != 200:
 		print('Page is not found')
 		exit(1)
-	print('Success')
+	print('~ Success ~\n')
 
-	soup = BeautifulSoup(website.text, "html.parser")
+	# Создаем BeautifulSoup объект, который принимает Текст с веб страницы.
+	# Используем встроенный в Питон парсер built-in HTML parser
+	soup = BeautifulSoup(website.text, 'html.parser')
+	# Находим элементы по HTML атрибутам на странице. берем целую строку fin-row
 	elements = soup.find_all('div', attrs={'data-test' : 'fin-row'})
 	for i in elements:
-		if i.find("div", attrs={'title' : sys.argv[2]}) is not None:
-			cols = i.find_all('div', {'data-test' : 'fin-col'})
-			return tuple([sys.argv[2], *[j.text for j in cols]])
+		if i.find('div', attrs={'title' : sys.argv[2]}) is not None:
+			cols = i.find_all('div', attrs={'data-test' : 'fin-col'})
+
+			# my_list = list()
+			
+			# result = [l + (''.join(l),) for l in list]
+			i = 0
+			for j in cols:
+				my_list.insert(i, j.text)
+				i = i + 1
+			
+			# return tuple([sys.argv[2], *[j.text for j in cols]])
+			return tuple(sys.argv[2], my_list)
 	print("statement name is not found")
 	exit(1)
 
@@ -35,7 +52,7 @@ def parse_info():
 
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	if len(sys.argv) != 3:
 		print('Wrong num of arg')
 		exit(1)

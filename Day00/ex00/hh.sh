c2c4eb91909884dbe09600c4790b6160303ad7e5
marlean <marlean@student.21-school.ru>
2022-09-16 11:43:39 +0300

    day00 ex00

---
+++ Day00/ex00/hh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+OUTPUT_FILE="./hh.json"
+VACANCY_AMOUNT="100"
+jq="../../../../homebrew/bin/jq"
+
+VACANCY_NAME="${1/ /+}"
+
+curl -k -H 'User-Agent: api-test-agent' -G "https://api.hh.ru/vacancies?text=$VACANCY_NAME&per_page=$VACANCY_AMOUNT" | $jq > $OUTPUT_FILE
